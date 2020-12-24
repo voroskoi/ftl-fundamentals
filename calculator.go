38b2c2b7283caad72cf0ad7d863351da7b131939
VÖRÖSKŐI András <voroskoi@gmail.com>
2020-12-24 15:51:49 +0100

    Add support for multiple parameters, adjust the tests too

--- calculator.go
+++ calculator.go
@@ -6,29 +6,40 @@ import (
 	"math"
 )
 
-// Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
-}
-
-// Subtract takes two numbers and returns the result of subtracting the second
-// from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// Multiply takes two numbers and returns the result of multiplying them together.
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// Divide takes two numbers and returns the result of dividing the first with the second.
-// If the second is 0, returns an error.
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("divide by 0 is not allowed")
+// Add adds all the other numbers to the first.
+func Add(a float64, b ...float64) float64 {
+	for _, v := range b {
+		a += v
 	}
-	return a / b, nil
+	return a
+}
+
+// Subtract subtracts all the other numbers from the first.
+func Subtract(a float64, b ...float64) float64 {
+	for _, v := range b {
+		a -= v
+	}
+	return a
+}
+
+// Multiply multiples all the given numbers together.
+func Multiply(a float64, b ...float64) float64 {
+	for _, v := range b {
+		a *= v
+	}
+	return a
+}
+
+// Divide divides the first number with all the rest.
+// If any other then the first equals to 0, returns an error.
+func Divide(a float64, b ...float64) (float64, error) {
+	for _, v := range b {
+		if v == 0 {
+			return 0, fmt.Errorf("divide by 0 is not allowed")
+		}
+		a /= v
+	}
+	return a, nil
 }
 
 // Sqrt takes a number and returns with the square root of it.
--- calculator_test.go
+++ calculator_test.go
@@ -10,18 +10,19 @@ import (
 var testCasesAdd = []struct {
 	desc string
 	a    float64
-	b    float64
+	b    []float64
 	want float64
 }{
-	{"add simple", 2, 2, 4},
-	{"add half", 2, 1.5, 3.5},
-	{"add negative", 2, -3, -1},
+	{"add simple", 2, []float64{2}, 4},
+	{"add half", 2, []float64{1.5}, 3.5},
+	{"add negative", 2, []float64{-3}, -1},
+	{"add multi", 3, []float64{2, 1}, 6},
 }
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testCasesAdd {
-		got := calculator.Add(tc.a, tc.b)
+		got := calculator.Add(tc.a, tc.b...)
 		if tc.want != got {
 			t.Errorf("%s: want %f, got %f", tc.desc, tc.want, got)
 		}
@@ -31,18 +32,19 @@ func TestAdd(t *testing.T) {
 var testCasesSubtract = []struct {
 	desc string
 	a    float64
-	b    float64
+	b    []float64
 	want float64
 }{
-	{"subtract simple", 4, 2, 2},
-	{"subtract half", 2, 1.5, 0.5},
-	{"subtract negative", 2, -3, 5},
+	{"subtract simple", 4, []float64{2}, 2},
+	{"subtract half", 2, []float64{1.5}, 0.5},
+	{"subtract negative", 2, []float64{-3}, 5},
+	{"subtract multi", 5, []float64{3, 5, -3}, 0},
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testCasesSubtract {
-		got := calculator.Subtract(tc.a, tc.b)
+		got := calculator.Subtract(tc.a, tc.b...)
 		if tc.want != got {
 			t.Errorf("%s: want %f, got %f", tc.desc, tc.want, got)
 		}
@@ -52,18 +54,19 @@ func TestSubtract(t *testing.T) {
 var testCasesMultiply = []struct {
 	desc string
 	a    float64
-	b    float64
+	b    []float64
 	want float64
 }{
-	{"multiply simple", 4, 2, 8},
-	{"multiply half", 2, 1.75, 3.5},
-	{"multiply negative", 2, -3, -6},
+	{"multiply simple", 4, []float64{2}, 8},
+	{"multiply half", 2, []float64{1.75}, 3.5},
+	{"multiply negative", 2, []float64{-3}, -6},
+	{"multiply multi", 2, []float64{2, -2, 2, -2, 2, -2, -2, -2}, -512},
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testCasesMultiply {
-		got := calculator.Multiply(tc.a, tc.b)
+		got := calculator.Multiply(tc.a, tc.b...)
 		if tc.want != got {
 			t.Errorf("%s: want %f, got %f", tc.desc, tc.want, got)
 		}
@@ -87,28 +90,30 @@ func TestMultiplyRand(t *testing.T) {
 var testCasesDivide = []struct {
 	desc string
 	a    float64
-	b    float64
+	b    []float64
 	want float64
 	err  bool
 }{
-	{"divide simple", 4, 2, 2, false},
-	{"divide half", 3.5, 1.75, 2, false},
-	{"divide negative", -6, -3, 2, false},
-	{"divide a zero", 0, 5, 0, false},
-	{"divide by zero", 3, 0, 0, true},
+	{"divide simple", 4, []float64{2}, 2, false},
+	{"divide half", 3.5, []float64{1.75}, 2, false},
+	{"divide negative", -6, []float64{-3}, 2, false},
+	{"divide a zero", 0, []float64{5}, 0, false},
+	{"divide by zero", 3, []float64{0}, 0, true},
+	{"divide multi", 27, []float64{3, -3, 3}, -1, false},
+	{"divide multi with zero", 81, []float64{5, 7, 1, 0, 3}, 0, true},
 }
 
 func TestDivide(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testCasesDivide {
-		got, err := calculator.Divide(tc.a, tc.b)
+		got, err := calculator.Divide(tc.a, tc.b...)
 		if err != nil {
 			if tc.err != true {
-				t.Error("no error reported when expected one")
+				t.Error("unexpected error returned")
 			}
 		} else {
 			if tc.err == true {
-				t.Error("unexpected error returned")
+				t.Error("no error reported when expected one")
 			}
 		}
 		if tc.want != got {
